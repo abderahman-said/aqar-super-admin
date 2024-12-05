fa6c6da3b222becc00d2d1c33abfeaf4b3f2ec01
abdorady417 <abdulrahman@puiux.org>
2024-12-05 13:55:17 +0200

    fd

--- src/pages/property/Properties.js
+++ src/pages/property/Properties.js
@@ -104,7 +104,6 @@ console.log("selectedType" ,selectedType)
               Add {ele.type}
             </button>
           </div>
-           
           <div className="Tabel_container">
             <div className={styles.Tabel} id="tableFirstItemStart">
               <DataTable
